4ca8fa4c3740ed20c3797307f10fc65eb42a4d39
karvel-code <velgithinji@gmail.com>
2021-10-25 11:11:39 +0300

    Correct rubocop errors

--- example_school_library_decorator/person.rb
+++ example_school_library_decorator/person.rb
@@ -4,7 +4,7 @@ class Person
   attr_accessor :name, :age, :rentals
   attr_reader :id
 
-  def initialize(age, name = 'Unknown', parent_permission = true)
+  def initialize(age, name = 'Unknown', parent_permission: true)
     @id = Random.rand(1..1000)
     @corrector = Corrector.new
     @age = age
@@ -14,7 +14,7 @@ class Person
   end
 
   def can_use_services?
-    @parent_permission || is_of_age?
+    @parent_permission || of_age?
   end
 
   def validate_name
@@ -23,7 +23,7 @@ class Person
 
   private
 
-  def is_of_age?
+  def of_age?
     @age >= 18
   end
 end
